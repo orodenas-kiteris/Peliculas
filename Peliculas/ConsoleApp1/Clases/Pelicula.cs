c6f611897b02bc95161b56def93b7f01a6a60d41
orodenas-kiteris <92022251+orodenas-kiteris@users.noreply.github.com>
2021-11-13 01:30:06 +0100

    Sin errores
    ok

---
+++ Peliculas/ConsoleApp1/Clases/Pelicula.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Clases
+{
+    public class Pelicula : ICloneable
+    {
+        public int CodigoPelicula { get; set; }
+        public string Nombre { get; set; }
+
+        public DateTime FechaEstreno { get; set; }
+
+        public Pelicula()
+        {
+
+        }
+
+        public Pelicula(int codigoPelicula, string nombre, DateTime fechaEstreno)
+        {
+            this.CodigoPelicula = codigoPelicula;
+            this.Nombre = nombre;
+            this.FechaEstreno = fechaEstreno;
+        }
+
+        public string MostarPelicula()
+        {
+            return $"La pelicula con id {this.CodigoPelicula} cuyo nombre es {this.Nombre} y su fecha de estreno {this.FechaEstreno.ToShortDateString()}";
+        }
+
+        public object Clone()
+        {
+            return (Pelicula) this.MemberwiseClone();
+        }
+    }
+}
+
---
+++ Peliculas/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Clases;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Error1();
+
+            Error2();
+
+            Error3();
+
+            Error4();
+
+            Console.ReadLine();
+        }
+
+       
+        private static void Error1()
+        {
+
+            Pelicula pelicula = new Pelicula(1, "Kill Bill Vol 1", new DateTime(2021, 08, 01));
+
+
+
+            Pelicula pelicula2;
+            pelicula2 = (Pelicula)pelicula.Clone();
+            
+            pelicula2.CodigoPelicula = 2;
+            pelicula2.Nombre = "Mulan";
+
+            Console.WriteLine("La pelicula1 es : " + pelicula.MostarPelicula());
+            Console.WriteLine("La pelicula2 es : " + pelicula2.MostarPelicula());
+            
+        }
+
+        private static void Error2()
+        {
+            int clavePelicula = 0;
+
+            Dictionary<int, Pelicula> diccionarioPelicula = new Dictionary<int, Pelicula>();
+
+            Pelicula pelicula = new Pelicula(1, "Kill Bill Vol 1", new DateTime(2021, 08, 01));
+
+            clavePelicula = pelicula.CodigoPelicula;
+            diccionarioPelicula.Add(clavePelicula, pelicula);
+            Console.WriteLine(diccionarioPelicula[1].MostarPelicula());
+
+            Pelicula pelicula2 = new Pelicula(2, "Kill Bill Vol 2", new DateTime(2021, 08, 01));
+            clavePelicula = pelicula2.CodigoPelicula;
+            diccionarioPelicula.Add(clavePelicula, pelicula);
+            Console.WriteLine(diccionarioPelicula[2].MostarPelicula());
+        }
+
+        private static void Error3()
+        {
+            Pelicula pelicula = new Pelicula(1, "Kill Bill Vol 1", new DateTime(2021, 01, 01));
+
+            Console.WriteLine(pelicula.MostarPelicula());
+
+        }
+
+        private static void Error4()
+        {
+            Pelicula pelicula = new Pelicula(100, "yo flipo", new DateTime(2021, 11, 12));
+
+            Console.WriteLine(pelicula.MostarPelicula());
+        }
+
+       
+
+    }
+}
+
